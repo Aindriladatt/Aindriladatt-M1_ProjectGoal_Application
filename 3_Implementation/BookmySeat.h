3c11de936dd52ecafd7262a4f414f79ed55e9a53
Aindriladatt <85006836+Aindriladatt@users.noreply.github.com>
2022-02-11 11:29:51 +0530

    Add files via upload

---
+++ 3_Implementation/BookmySeat.h
@@ -0,0 +1,9 @@
+#include<stdio.h>      /* The idea of this project was taken from code projects.I tried to put a login system differently */
+#include<stdlib.h>
+#include<string.h>
+
+void businfo(void);
+void reserve(void);
+void printticket(char name[],int,int,float);
+void busdet()
+float payment(int,int);
